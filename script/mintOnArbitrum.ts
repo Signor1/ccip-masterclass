4699791676dcc3ac14ac728d9788d3deeb55136d
signor1 <emmanuelomemgboji@gmail.com>
2024-08-18 14:05:55 +0100

    feat: added another script for minting after enabling chains

---
+++ script/mintOnArbitrum.ts
@@ -0,0 +1,32 @@
+import { ethers, network } from "hardhat";
+import { Wallet } from "ethers";
+import { XNFT, XNFT__factory } from "../typechain-types";
+
+async function main() {
+  if (network.name !== `arbitrumSepolia`) {
+    console.error(`Must be called from Arbitrum Sepolia`);
+    return 1;
+  }
+
+  const privateKey = process.env.PRIVATE_KEY!;
+  const rpcProviderUrl = process.env.ARBITRUM_SEPOLIA_RPC_URL;
+
+  const provider = new ethers.JsonRpcProvider(rpcProviderUrl);
+  const wallet = new Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  const xNftAddressArbitrumSepolia = `0x9aF75efA7386a5cfEB5119359f65D9A45d3D94b8`;
+
+  const xNft: XNFT = XNFT__factory.connect(xNftAddressArbitrumSepolia, signer);
+
+  const tx = await xNft.mint();
+
+  console.log(`Transaction hash: ${tx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+// Transaction hash: 0x3085cc7026ffdfd3e7d4944be833e804b7dc73578fe66bade8bd5866929a1e8a
